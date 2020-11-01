c120c65d36e52619de759856a4684c958706885a
CPDEZ08 <CPDEZ08@outlook.com>
2020-11-01 02:17:27 +0000

    Homework

---
+++ inspect_hawaii.py
@@ -0,0 +1,21 @@
+## Step 1 - Climate Analysis and Exploration
+
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from pprint import pprint
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+
+inspector = inspect(engine)
+
+table_names = inspector.get_table_names()
+# print(table_names)
+
+for table_name in table_names:
+    print(table_name)
+    columns = inspector.get_columns(table_name)
+    pprint(columns)
+
+
